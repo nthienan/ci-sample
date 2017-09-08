527760436ad512df7f28eb809b4ce912533967a6
An Nguyen <nthienan@tma.com.vn>
2017-09-08 17:03:32 +0700

    Add new api
    Summary: Add new api
    
    Test Plan: Done
    
    Reviewers: bahuy
    
    Reviewed By: bahuy
    
    Subscribers: sonarqube, jenkins
    
    Differential Revision: http://192.168.88.211/D1

--- src/main/java/com/tma/ci4tma/Application.java
+++ src/main/java/com/tma/ci4tma/Application.java
@@ -2,6 +2,7 @@ package com.tma.ci4tma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class Application {
     @RequestMapping("/")
     public String home() {
         return "Hello Team ! How are you?";
+    }
+
+    @RequestMapping("/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello " + name;
     }
 
     public static void main(String[] args) {
